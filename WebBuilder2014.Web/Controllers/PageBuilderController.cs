2421bd533c977476fc00a05dc7dd74612dc8c6c5
steven_caochao@hotmail.com <steven_caochao@hotmail.com>
2014-09-26 20:56:13 +1000

    folder

---
+++ WebBuilder2014.Web/Controllers/PageBuilderController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBuilder2014.Web.Controllers
+{
+    public class PageBuilderController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Page/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Page/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Page/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Page/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Page/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Page/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Page/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebBuilder2014.Web/api/PageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebBuilder2014.Web.api
+{
+    public class PageController : ApiController
+    {
+        // GET: api/Page
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Page/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Page
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Page/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Page/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
