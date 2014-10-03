130b5244f508699a74e8950c75a1310fc87eca8c
stevenccoh <steven_caochao@hotmail.com>
2014-10-03 17:25:42 +1000

    drag

--- WebBuilder2014.Web/Scripts/pagebuilder.js
+++ WebBuilder2014.Web/Scripts/pagebuilder.js
@@ -1,5 +1,13 @@
 ﻿$(function () {
+    //pagebuilder
+    $(".wb_sortable").sortable({
+        revert: true
+    });
     //toolbox
     $("#wb_toolbox").draggable({ handle: ".title" });
     $("#wb_toolbox .accordion").accordion();
+    $('#wb_toolbox .wb_draggable').draggable({
+        connectToSortable: "#pagebuilder .wb_sortable",
+        helper: "clone"
+    }).disableSelection();
 });
