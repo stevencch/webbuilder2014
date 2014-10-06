ef5fb1d2d07845fa7f2343febb8206e629f6a1ad
stevenccoh <steven_caochao@hotmail.com>
2014-10-06 17:21:16 +1100

    add modal

--- WebBuilder2014.Web/Scripts/pagebuilder.js
+++ WebBuilder2014.Web/Scripts/pagebuilder.js
@@ -1,7 +1,21 @@
-﻿/*global*/
+﻿/*global function*/
+if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+        return this.replace(/^[\s\xA0]+|[\s\xA0]+$/g, '');
+    };
+}
+
+String.prototype.short = function (i) {
+    if (this.length < i) {
+        i = this.length;
+    }
+    return this.substr(0, i);
+};
+/*global variable*/
 var uid = 0;
 var currentSection = null;
 var tempContent = '';
+var currentNode = null;
 
 
 
@@ -10,14 +24,79 @@ $(function () {
     //pagebuilder
     $(".wb_sortable").sortable({
         revert: true,
+        placeholder: "ui-state-placeholder",
         stop: sortableStop
     });
+    //context menu
+    //$.contextMenu({
+    //    selector: '#wb_pagebuilder .wb_draggable',
+    //    //trigger: 'hover',
+    //    //delay: 200,
+    //    //autoHide: true,
+    //    zIndex:9999,
+    //    build: function ($trigger, e) {
+    //        $('.selectedNode').removeClass('selectedNode');
+    //        $trigger.addClass("selectedNode");
+    //        return {
+    //            callback: function (key, options) {
+    //                switch (key) {
+    //                    case 'text':
+
+    //                        break;
+    //                }
+    //                //window.console && console.log(m) || alert(m);
+    //            },
+    //            items: {
+    //                "edit text": { name: "text", icon: "edit" },
+    //                "edit image": { name: "image", icon: "edit" },
+    //                "edit icon": { name: "icon", icon: "edit" },
+    //                "sep1": "---------",
+    //                "delete this": { name: "delete", icon: "delete" },
+    //            }
+    //        };
+    //    }
+    //});
+    $('#wb_pagebuilder').delegate('.wb_draggable','click', function () {
+        currentNode = $(this);
+        $('.selectedNode').removeClass('selectedNode');
+        currentNode.addClass("selectedNode");
+    });
+
     //toolbox
     $("#wb_toolbox").draggable({ handle: ".title" });
     $("#wb_toolbox .accordion").accordion();
     $('.wb_draggable').draggable({
         connectToSortable: ".wb_sortable",
         helper: "clone"
+    });
+
+    //toolbar
+    $('#btnEditText').click(function () {
+        var html = '<ul>';
+        var textList = currentNode.find("*[txtid]");
+        var active = '';
+        for (var i = 0; i < textList.length; i++) {
+            var txtid = $(textList[i]).attr('txtid');
+            if (txtid == currentNode.attr('txtid')) {
+                active = ' active';
+            }
+            else {
+                active = '';
+            }
+            html += '<li class="list-group-item' + active + '" txtid="' + txtid + '">' + textList[i].innerHTML.replace(/<[^>]*>/g, '').short(40) + '</li>';
+        }
+        html += '</ul>';
+        $('#textPanel').html(html);
+
+        $('#wb_EditTextModal').modal({
+            backdrop: false,
+            show: true
+        });
+    });
+
+    //modal
+    $("#wb_EditTextModal").draggable({
+        handle: ".modal-title"
     });
 });
 
@@ -71,7 +150,7 @@ function triggerJs(code) {
 
 function sortableStop(event, ui) {
     currentSection = ui.item;
-    $.get("/api/page/" + currentSection.attr('sid'), function(data) {
+    $.get("/api/page/" + currentSection.attr('sid'), function (data) {
         tempContent = '';
         getHtml(data);
         currentSection.html(tempContent);
@@ -79,10 +158,11 @@ function sortableStop(event, ui) {
         //layout
         currentSection.find('.wb_sortable').sortable({
             revert: true,
+            placeholder: "ui-state-placeholder",
             stop: sortableStop
         });
 
-    }).fail(function() {
+    }).fail(function () {
         alert('fail');
     });
 }
